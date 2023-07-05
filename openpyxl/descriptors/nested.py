f46883735833fc038ee5ce9938c211418f2970b0
Charlie Clark <charlie.clark@clark-consulting.eu>
2023-07-05 19:35:31 +0200

    Remove unused import
    --HG--
    branch : 3.1

--- openpyxl/descriptors/nested.py
+++ openpyxl/descriptors/nested.py
@@ -13,9 +13,7 @@ from .base import (
     Float,
     Integer,
     String,
-    Text,
     )
-from .sequence import Sequence
 from openpyxl.compat import safe_string
 from openpyxl.xml.functions import Element, localname, whitespace
 
